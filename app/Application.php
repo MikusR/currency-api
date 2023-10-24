14fd9cb55aa2aaaf332ec437aec98edb23cc799b
MikusR <mikus.rakis@gmail.com>
2023-10-24 17:32:13 +0300

    Working

--- app/Application.php
+++ app/Application.php
@@ -16,15 +16,7 @@ class Application
 
     public function run(): void
     {
-        //   $this->exchange = $this->buildExchange();
-        $this->exchange = new Exchange(
-            '1698151863',
-            [
-                'USD' => new Currency('USD', 'dol', 1.06),
-                'EUR' => new Currency('EUR', 'eu', 1)
-            ]
-        );
-        echo $this->exchange->exchange('USD', 100, 'EUR') . PHP_EOL;
+        $this->exchange = $this->buildExchange();
         $this->ui();
     }
 
@@ -47,33 +39,39 @@ class Application
 
     public function ui(): void
     {
-        echo "Date of rates: " . $this->exchange->getTimestamp() . PHP_EOL;
-        echo "1. to do currency conversion\n";
-        echo "any other key to exit ";
-        $choice = (int)readline();
+        while (true) {
+            echo "Date of rates: " . $this->exchange->getTimestamp() . PHP_EOL;
+            echo "1. to do currency conversion\n";
+            echo "any other key to exit ";
+            $choice = (int)readline();
 
-        switch ($choice) {
-            case 1:
-                echo "enter amount and currency (ex: 100 USD) ";
-                $userInput = explode(' ', readline());
-                $amount = (int)$userInput[0];
-                $currency = strtoupper($userInput[1]);
-                if (array_key_exists($currency, $this->isoCodes->get()) === false) {
-                    echo "no such currency\n";
+            switch ($choice) {
+                case 1:
+                    echo "enter amount and currency (ex: 100 USD) ";
+                    $userInput = explode(' ', readline());
+                    $amount = (int)$userInput[0];
+                    if (($amount <= 0) || (is_nan($amount))) {
+                        echo "enter positive number\n";
+                        break;
+                    };
+                    $currency = strtoupper($userInput[1]);
+                    if (array_key_exists($currency, $this->isoCodes->get()) === false) {
+                        echo "no such currency\n";
+                        break;
+                    }
+                    $currencyTo = strtoupper(readline("Currency to "));
+                    if (array_key_exists($currencyTo, $this->isoCodes->get()) === false) {
+                        echo "no such currency\n";
+                        break;
+                    }
+                    echo "You want to convert $amount {$this->isoCodes->get()[$currency]}";
+                    echo " into {$this->isoCodes->get()[$currencyTo]} \nYou get:\n";
+                    echo ($this->exchange->exchange($currency, 100 * $amount, $currencyTo)) / 100
+                        . " {$this->isoCodes->get()[$currencyTo]}\n";
                     break;
-                }
-                $currencyTo = strtoupper(readline("Currency to "));
-                if (array_key_exists($currencyTo, $this->isoCodes->get()) === false) {
-                    echo "no such currency\n";
-                    break;
-                }
-                echo "You want to convert $amount {$this->isoCodes->get()[$currency]}\n";
-                echo "Into {$this->isoCodes->get()[$currencyTo]}\n You get:\n";
-                echo ($this->exchange->exchange($currency, 100 * $amount, $currencyTo)) / 100
-                    . " {$this->isoCodes->get()[$currencyTo]}\n";
-
-            default:
-                die;
+                default:
+                    die;
+            }
         }
     }
 
